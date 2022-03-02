cad086be0435eedce0d8f4bdc388cd9f27851617
leandrocgsi <leandrocgsi@gmail.com>
2022-03-02 16:43:06 -0300

    Generics in Kotlin

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0505/Generics.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0505/Generics.kt
@@ -1,8 +1,14 @@
-package br.com.erudio.section05._0504
+package br.com.erudio.section05._0505
 
 fun main() {
 
-    val myCar = Car()
-    myCar.start()
-    println("My car's make = ${myCar.automakerName}")
+    val maxInt: Int = max(42, 99)
+    val maxLong: Long = max(123_456_789L, 999_999_999L)
+    val maxByte: Byte = max((-128).toByte(), (127).toByte())
+    val maxString: String = max("Alpha", "Omega")
+
+    println("The max Int = $maxInt")
+    println("The max Long = $maxLong")
+    println("The max Byte = $maxByte")
+    println("The max String = $maxString")
 }
--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0505/Max.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0505/Max.kt
@@ -1,4 +1,6 @@
 package br.com.erudio.section05._0505
 
-class Max {
+fun <T: Comparable<T>> max(param1: T, param2: T) : T {
+    val result = param1.compareTo(param2)
+    return if (result > 0) param1 else param2
 }
