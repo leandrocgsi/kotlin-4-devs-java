ad9ccf7b281628ebecb827594b0724e69d11577a
leandrocgsi <leandrocgsi@gmail.com>
2022-03-02 12:22:41 -0300

    Classes in Kotlin

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0502/Classes.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0502/Classes.kt
@@ -1,4 +1,6 @@
 package br.com.erudio.section05._0502
 
-class Classes {
+fun main() {
+    var person1 = Person("Peter", "Park")
+    var person2 = Person("Stan", "Lee", "Martin")
 }
--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0502/Person.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0502/Person.kt
@@ -1,4 +1,13 @@
 package br.com.erudio.section05._0502
 
-class Person {
+// class Person constructor (firstName: String, lastName: String) {
+// class Person (firstName: String, lastName: String) {
+class Person internal constructor (firstName: String, lastName: String) {
+
+    init {
+        println("Created a person named $firstName $lastName")
+    }
+
+    constructor (firstName: String, lastName: String, middleName: String):
+            this(firstName, lastName)
 }
