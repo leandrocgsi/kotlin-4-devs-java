8c3308a399e5f879ac134da08ef456a838d7f7f0
leandrocgsi <leandrocgsi@gmail.com>
2022-03-02 16:42:38 -0300

    Classes in Kotlin

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0504/Car.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0504/Car.kt
@@ -1,4 +1,8 @@
 package br.com.erudio.section05._0504
 
-class Car {
+class Car : Vehicle {
+
+    override val automakerName = "Honda"
+
+    override fun getDoors() = 5
 }
--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0504/Vehicle.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section05/_0504/Vehicle.kt
@@ -1,4 +1,20 @@
 package br.com.erudio.section05._0504
 
 interface Vehicle {
+
+    val automakerName: String
+
+    fun start() {
+        println("Vruuuuuuuuuummmmmmm")
+    }
+
+    fun stop() {
+        println("Brake noise")
+    }
+
+    fun getKmPerLiter() : Int {
+        return 17
+    }
+
+    fun getDoors(): Int
 }
