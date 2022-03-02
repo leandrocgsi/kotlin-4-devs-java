a5f6315b944581d3eeb3a37e629945d47a0fc6c8
leandrocgsi <leandrocgsi@gmail.com>
2022-03-02 12:21:48 -0300

    FizzBuzz Challenge

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section04/_0412/FizzBuzz.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section04/_0412/FizzBuzz.kt
@@ -1,5 +1,31 @@
-package br.com.erudio
+package br.com.erudio.section04._0412
 
-fun main() {
+fun fizzBuzz1(n: Int): List<String> {
+    val list = mutableListOf<String>()
 
+    for (it in  1..n) {
+        val item = when {
+            it % 3 == 0 && it % 5 == 0 -> "FizzBuzz"
+            it % 3 == 0 -> "Fizz"
+            it % 5 == 0 -> "Buzz"
+            else -> it.toString()
+        }
+        list.add(item)
+    }
+    return list
+}
+
+fun fizzBuzz(n: Int): List<String> {
+    val list = mutableListOf<String>()
+
+    (1..n).forEach {
+        val item = when {
+            it % 3 == 0 && it % 5 == 0 -> "FizzBuzz"
+            it % 3 == 0 -> "Fizz"
+            it % 5 == 0 -> "Buzz"
+            else -> it.toString()
+        }
+        list.add(item)
+    }
+    return list
 }
