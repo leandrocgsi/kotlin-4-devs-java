1b2760c6c6a009d761eb837a7af8c57746272c6a
leandrocgsi <leandrocgsi@gmail.com>
2022-03-02 16:44:34 -0300

    Using Java Code on Kotlin Code

--- kotlin-4-devs-java/src/main/java/br/com/erudio/section06/_0602/Animal.java
+++ kotlin-4-devs-java/src/main/java/br/com/erudio/section06/_0602/Animal.java
@@ -1,2 +1,34 @@
-package br.com.erudio.section06._0602;public class Animal {
+package br.com.erudio.section06._0602;
+
+public class Animal {
+
+    private final String name;
+    private final String kind;
+    private Integer weight;
+
+    public Animal(String name, String kind, Integer weight) {
+        this.name = name;
+        this.kind = kind;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String show(){
+        return name + " is a " + kind + " and weighs " + weight + " kg!";
+    }
 }
--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section06/_0602/HelloJavaFromKotlin.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section06/_0602/HelloJavaFromKotlin.kt
@@ -1,4 +1,14 @@
 package br.com.erudio.section06._0602
 
-class HelloJavaFromKotlin {
+fun main() {
+
+    val cat = Animal("Garfield", "cat", 10)
+    println(cat.show())
+    val dog = Animal("Totó", "dog", 30)
+    println(dog.show())
+
+    cat.weight = 13
+    println(cat.show())
+    // cat.kind = "horse"
+
 }
