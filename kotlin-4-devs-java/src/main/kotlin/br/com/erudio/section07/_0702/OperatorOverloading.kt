3354ce33659a3e0abed7aecf7c8e89ddef902cda
leandrocgsi <leandrocgsi@gmail.com>
2022-03-04 14:17:20 -0300

    Operator Overloading

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section07/_0702/OperatorOverloading.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section07/_0702/OperatorOverloading.kt
@@ -1,4 +1,9 @@
 package br.com.erudio.section07._0702
 
-class OperatorOverloading {
+fun main() {
+
+    val c1 = Coordinates(200, 100)
+    val c2 = Coordinates(1000, 2000)
+    val c3 = c1 + c2
+    println(c3)
 }
