5c864c3960adf90179c91ce5395e1643c7d5397a
leandrocgsi <leandrocgsi@gmail.com>
2022-03-04 14:19:34 -0300

    Operator Overloading Challenge

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section07/_0707/Coordinates.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section07/_0707/Coordinates.kt
@@ -1,8 +1,8 @@
-package br.com.erudio.section07._0702
+package br.com.erudio.section07._0707
 
 data class Coordinates(var x: Int, var y: Int) {
 
-    operator fun plus(other: Coordinates) : Coordinates {
-        return  Coordinates(x + other.x, y + other.y)
-    }
+    operator fun plus(other: Coordinates) : Coordinates = Coordinates(x + other.x, y + other.y)
+
+    operator fun minus(other: Coordinates) : Coordinates = Coordinates(x - other.x, y - other.y)
 }
