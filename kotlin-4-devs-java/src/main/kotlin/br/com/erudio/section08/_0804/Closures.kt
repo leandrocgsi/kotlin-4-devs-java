dce79388ca57cdd24746ca8cb2a7b1581f513ca4
leandrocgsi <leandrocgsi@gmail.com>
2022-03-06 11:24:54 -0300

    Closures in Kotlin

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0804/Closures.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0804/Closures.kt
@@ -1,4 +1,19 @@
 package br.com.erudio.section08._0804
 
-class Closures {
+fun closureMaker(): () -> Unit {
+    var num = 0
+    return { println(num++) }
+}
+
+fun main() {
+    val myCounter1 = closureMaker()
+    val myCounter2 = closureMaker()
+
+    myCounter1()
+    myCounter1()
+    myCounter1()
+    myCounter1()
+    myCounter2()
+    myCounter2()
+    myCounter1()
 }
