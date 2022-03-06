1e83cacb45487166d6db36267850742bb89d1608
leandrocgsi <leandrocgsi@gmail.com>
2022-03-06 12:06:30 -0300

    Sequencies in Kotlin

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0805/Sequencies.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0805/Sequencies.kt
@@ -1,5 +1,21 @@
 package br.com.erudio.section08._0805
 
 fun main() {
-    
+
+    var students = getStudents()
+
+    val sqStudents = students.drop(1).take(3).toList()
+    println("Sequenced students: $sqStudents")
+
+    val numbers = generateSequence(100) { it + 1 }
+    println("Numbers: ${numbers.drop(5).take(20).toList()}")
+
+    val squares = generateSequence(1) { it + 1  }.map{ it * it}
+    val evenSquares = squares.filter { it % 2 == 0}
+    println("Even Squares: ${evenSquares.take(5).toList()}")
+
+    val fibonacci = generateSequence(1 to 1)
+                        { it.second to it.first + it.second }.map { it.first }
+
+    println("Fibonacci: ${ fibonacci.take(15).toList() }")
 }
