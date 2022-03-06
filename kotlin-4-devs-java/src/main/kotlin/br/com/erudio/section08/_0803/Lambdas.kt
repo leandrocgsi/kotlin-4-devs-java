2100bc9f61f2c65b9f1f673dced5c97ec09c8d92
leandrocgsi <leandrocgsi@gmail.com>
2022-03-06 11:14:21 -0300

    Lambdas in Kotlin

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0803/Lambdas.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0803/Lambdas.kt
@@ -1,4 +1,11 @@
 package br.com.erudio.section08._0803
 
-class Lambdas {
+fun main() {
+    val students = getStudents()
+
+    val combos = students.map { a -> "${a.name} : ${a.age}"}
+    println("Combos: $combos")
+
+    println("The oldest student is: ${students.maxByOrNull { it.age }}")
+    println("Student with longest name is: ${students.filter { it.name.length > 5 }}")
 }
