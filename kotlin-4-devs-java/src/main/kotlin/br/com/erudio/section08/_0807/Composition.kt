a8949816fff9141115660ba674d65ba3dd0362af
leandrocgsi <leandrocgsi@gmail.com>
2022-03-06 12:08:25 -0300

    Composition in Kotlin

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0807/Composition.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0807/Composition.kt
@@ -1,8 +1,19 @@
-package br.com.erudio.section08._0806
+package br.com.erudio.section08._0807
 
-import org.funktionale.collections.destructured
+import org.funktionale.composition.compose
+import org.funktionale.composition.forwardCompose
 
 fun main() {
-    val (head, tail) = listOf(1, 2, 3).destructured()
-    println("Head = $head and tail = $tail")
+
+    val add5 = {i: Int -> i + 5}
+    val multiplyBy2 = {i: Int -> i * 2}
+    val multiplyBy2andAdd5 = add5 compose multiplyBy2
+
+    val composeResult = multiplyBy2andAdd5(10)
+    println("multiplyBy2andAdd5(10) = $composeResult")
+
+    val add5AndMultiplyBy2 = add5 forwardCompose multiplyBy2
+
+    val forwardComposeResult = add5AndMultiplyBy2(10)
+    println("add5AndMultiplyBy2(10) = $forwardComposeResult")
 }
