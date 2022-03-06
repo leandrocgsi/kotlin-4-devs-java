35af554a80d65b3e2d5e784958f751b3db728c6f
leandrocgsi <leandrocgsi@gmail.com>
2022-03-06 12:40:22 -0300

    Memoization in Kotlin

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0810/Memoization.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0810/Memoization.kt
@@ -1,19 +1,28 @@
-package br.com.erudio.section08._0809
+package br.com.erudio.section08._0810
 
-import org.funktionale.partials.partially1
-import org.funktionale.partials.partially3
+import org.funktionale.memoization.memoize
 
 fun main() {
 
-    //    _X_
-    val prefixAndPostFix: (String, String, String) -> String =
-        {prefix: String, x: String, postfix: String
-            -> "${prefix}${x}${postfix}"}
+    var fibonacci: (Long) -> Long = {it}
 
-    val prefixAndAlert: (String, String) -> String =
-        prefixAndPostFix.partially3("!")
+    fibonacci = {n: Long ->
+        if (n < 2) n else fibonacci(n - 1) + fibonacci(n - 2)
+    }
 
-    val hello: (String) -> String = prefixAndAlert.partially1("Hello, ")
+    var m: (Long) -> Long = {it}
 
-    println(hello("FunKTionale"))
+    m = {n: Long ->
+        if (n < 2) n else m(n - 1) + m(n - 2)
+    }.memoize()
+
+    println("Calling fibonacci: ${timeElapsed { fibonacci (40)}} ms")
+    println("Calling memoized fibonacci: ${timeElapsed { m (40)}} ms")
+}
+
+fun timeElapsed(body: () -> Unit): Long {
+    val start = System.currentTimeMillis()
+    body()
+    val end = System.currentTimeMillis()
+    return end - start
 }
