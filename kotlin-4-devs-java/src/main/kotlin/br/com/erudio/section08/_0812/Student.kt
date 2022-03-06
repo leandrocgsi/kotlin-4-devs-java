6c788b5a671319eb417d180c410cf47993d4dd64
leandrocgsi <leandrocgsi@gmail.com>
2022-03-06 16:26:04 -0300

    Shortest Student Name Challenge

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0812/Student.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0812/Student.kt
@@ -1,3 +1,7 @@
 package br.com.erudio.section08._0812
 
 data class Student(val name: String, var age: Int)
+
+fun findTheShortestStudentName(students: List<Student>): Student? {
+    return students.minByOrNull { it.name.length }
+}
